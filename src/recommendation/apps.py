05dcab271632f28391cbc88fcc2a44b54703e39d
Suman Gole <gole35830@gmail.com>
2023-10-03 20:45:21 +0545

    refactor: change the name of the app

--- src/recommendation/apps.py
+++ src/recommendation/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class RecommdationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'recommdation'
+    name = 'recommendation'
