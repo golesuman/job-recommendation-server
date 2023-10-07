1a20b874d5da6f2d1cc9be72f57670953424ddf9
Suman Gole <gole35830@gmail.com>
2023-10-07 15:00:38 +0545

    feat(api): user login

--- src/account/views.py
+++ src/account/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
-
+from django.contrib.auth import authenticate, login
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.authtoken.models import Token
 # Create your views here.
+from rest_framework.views import  APIView
+
+
+class UserLoginView(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            token, _ = Token.objects.get_or_create(user=user)
+            return Response({'token': token.key, 'user_id': user.id}, status=status.HTTP_200_OK)
+        else:
+            return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
