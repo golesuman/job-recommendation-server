2cbe3c696e1b08703d287b5c79e4023cf0ab585d
Suman Gole <gole35830@gmail.com>
2023-11-04 17:07:19 +0545

    fix(api): remove appending slash

--- src/backend_api/settings.py
+++ src/backend_api/settings.py
@@ -160,4 +160,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-APPEND_SLASH=False
+# APPEND_SLASH=False
