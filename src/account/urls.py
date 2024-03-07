3e8d1a919417804fd3e8a20f3e7f56b8a533a837
Suman Gole <gole35830@gmail.com>
2024-03-07 11:59:21 +0545

    chore: remove trailing slash

--- src/account/urls.py
+++ src/account/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from .views import UserLoginView, UserLogoutView, UserProfileAPI, UserSignUpAPI
 
 urlpatterns = [
-    path("login/", UserLoginView.as_view(), name="user_login"),
-    path("logout/", UserLogoutView.as_view(), name="user_logout"),
-    path("profile/", UserProfileAPI.as_view(), name="user_profile"),
-    path("signup/", UserSignUpAPI.as_view(), name="user_signup"),
+    path("login", UserLoginView.as_view(), name="user_login"),
+    path("logout", UserLogoutView.as_view(), name="user_logout"),
+    path("profile", UserProfileAPI.as_view(), name="user_profile"),
+    path("signup", UserSignUpAPI.as_view(), name="user_signup"),
 ]
