0d038a6540bbef96a50d91f1e2fe7533c24e3c51
Suman Gole <gole35830@gmail.com>
2024-03-12 20:26:49 +0545

    refactor: use jobs object to filter the jobs based on list of ids

--- src/recommendation/home_view.py
+++ src/recommendation/home_view.py
@@ -44,7 +44,7 @@ class RecommendationView(views.APIView):
                 model="cosine", job_listings_dict=job_listings_dict, data=profile_
             )
 
-        jobs = Job.objects.filter(id__in=job_ids)
+        jobs = job_listings.filter(id__in=job_ids)
         serializer = JobPostSerializer(jobs, many=True)
         return Response({"data": serializer.data}, status=status.HTTP_200_OK)
 
