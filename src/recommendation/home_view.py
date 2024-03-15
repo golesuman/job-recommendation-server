1619afadedec7ed1fcca4d0c519b94f9ffc7d475
Suman Gole <gole35830@gmail.com>
2024-03-15 20:30:12 +0545

    feat: use caching

--- src/recommendation/home_view.py
+++ src/recommendation/home_view.py
@@ -5,7 +5,7 @@ from account.models import Interaction, UserProfile
 
 from .serializers import JobDetailsSerializer, JobPostSerializer
 from .algorithms_v2 import get_recommendations
-
+from django.core.cache import cache
 
 INTERACTION_LIMIT = 10
 THRESHOLD = 0.3
@@ -21,11 +21,14 @@ class RecommendationView(views.APIView):
         interactions = Interaction.objects.filter(user_id=user_id)
         user_profile = UserProfile.objects.filter(user_id=user_id).first()
 
-        job_listings = Job.objects.all()
+        job_listings_dict = cache.get("job_listings_dict")
 
-        job_listings_dict = {
-            str(job.id): job.title + "," + job.description for job in job_listings
-        }
+        if job_listings_dict is None:
+            job_listings = Job.objects.all()
+            job_listings_dict = {
+                str(job.id): job.title + "," + job.description for job in job_listings
+            }
+            cache.set("job_listings_dict", job_listings_dict)
 
         if interactions.count() > INTERACTION_LIMIT:
             interaction_history = [
@@ -39,14 +42,15 @@ class RecommendationView(views.APIView):
             )
 
         else:
-            skills = user_profile.skills if user_profile.skills else ""
-            bio = user_profile.bio if user_profile.bio else ""
-            profile_ = bio + skills
-            job_ids = self.get_results(
-                model="cosine", job_listings_dict=job_listings_dict, data=profile_
-            )
+            if user_profile.skills:
+                skills = user_profile.skills
+                job_ids = self.get_results(
+                    model="cosine", job_listings_dict=job_listings_dict, data=skills
+                )
+            else:
+                job_ids = []
 
-        jobs = job_listings.filter(id__in=list(set(job_ids)))
+        jobs = Job.objects.filter(id__in=list(set(job_ids)))
         serializer = JobDetailsSerializer(jobs, many=True)
         return Response({"data": serializer.data}, status=status.HTTP_200_OK)
 
