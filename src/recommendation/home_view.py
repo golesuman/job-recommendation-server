152b426efdcac902217c643f0a1d39b911a62cda
Suman Gole <gole35830@gmail.com>
2024-03-15 19:35:22 +0545

    perf: improve the job recommendations

--- src/recommendation/home_view.py
+++ src/recommendation/home_view.py
@@ -8,6 +8,7 @@ from .algorithms_v2 import get_recommendations
 
 
 INTERACTION_LIMIT = 10
+THRESHOLD = 0.3
 
 
 class RecommendationView(views.APIView):
@@ -23,22 +24,23 @@ class RecommendationView(views.APIView):
         job_listings = Job.objects.all()
 
         job_listings_dict = {
-            str(job.id): f"{job.title, job.description}" for job in job_listings
+            str(job.id): job.title + "," + job.description for job in job_listings
         }
 
         if interactions.count() > INTERACTION_LIMIT:
-            user_search_history = [
-                interaction.job.title for interaction in interactions
+            interaction_history = [
+                interaction.job.title + "," + interaction.job.description
+                for interaction in interactions
             ]
             job_ids = self.get_results(
                 model="cosine",
                 job_listings_dict=job_listings_dict,
-                data=user_search_history,
+                data=interaction_history,
             )
 
         else:
-            skills = user_profile.skills.split(",") if user_profile.skills else []
-            bio = user_profile.bio if user_profile.bio else []
+            skills = user_profile.skills if user_profile.skills else ""
+            bio = user_profile.bio if user_profile.bio else ""
             profile_ = bio + skills
             job_ids = self.get_results(
                 model="cosine", job_listings_dict=job_listings_dict, data=profile_
@@ -48,13 +50,23 @@ class RecommendationView(views.APIView):
         serializer = JobDetailsSerializer(jobs, many=True)
         return Response({"data": serializer.data}, status=status.HTTP_200_OK)
 
+    def recommendation_service(self, interaction, job_listings_dict, model, job_ids):
+        recommendation = get_recommendations(
+            interaction, job_listings_dict, model=model
+        )
+        for doc, similarity in recommendation:
+            if similarity > THRESHOLD:
+                job_ids.append(doc)
+
     def get_results(self, model, data, job_listings_dict):
         job_ids = []
-        for interaction in data:
-            recommendation = get_recommendations(
-                interaction, job_listings_dict, model=model
+        if isinstance(data, list):
+            for interaction in data:
+                self.recommendation_service(
+                    interaction, job_listings_dict, model=model, job_ids=job_ids
+                )
+        else:
+            self.recommendation_service(
+                data, job_listings_dict, model=model, job_ids=job_ids
             )
-            for doc, similarity in recommendation:
-                if similarity > 0.5:
-                    job_ids.append(doc)
         return job_ids
